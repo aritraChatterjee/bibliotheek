0e4bed6fe9b05cc7e624e3eae612d5799e41721b
aritraChatterjee <aritra.cnw@gmail.com>
2021-03-06 15:23:26 +0100

    refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import theme from './Theme';
-import Header from './Header';
-import Footer from './Footer';
-import About from './About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
     return (
