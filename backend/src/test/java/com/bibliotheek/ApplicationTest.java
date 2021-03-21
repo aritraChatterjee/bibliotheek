f0a9fbb614c64befe0b9dcc2d2c97a2f536a7354
aritraChatterjee <aritra.cnw@gmail.com>
2021-03-21 13:25:04 +0100

    BIB-6: fix for failing tests

--- backend/src/test/java/com/bibliotheek/ApplicationTest.java
+++ backend/src/test/java/com/bibliotheek/ApplicationTest.java
@@ -2,12 +2,10 @@ package com.bibliotheek;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ApplicationTest {
 
     @Test
