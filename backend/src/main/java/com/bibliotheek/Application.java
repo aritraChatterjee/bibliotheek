fd924546ec3565979aec3ca2dbc571a2785d41c2
aritraChatterjee <aritra.cnw@gmail.com>
2021-06-27 03:38:15 +0200

    BIB-16: added configuration to deploy on Jboss server 7.3.0

--- backend/src/main/java/com/bibliotheek/Application.java
+++ backend/src/main/java/com/bibliotheek/Application.java
@@ -2,9 +2,25 @@ package com.bibliotheek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * In order to deploy the application in an application server like Jboss,
+ * the Application class extends {@link SpringBootServletInitializer}
+ */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    /*
+     * This method is overridden to configure for deploying the application
+     * in a separate application server.
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
